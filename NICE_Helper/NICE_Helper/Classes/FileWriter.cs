b5f54546b79dfe1741fd1ab953da5fa059bf0fde
Simportant <simportant@virginmedia.com>
2019-07-19 10:54:16 +0100

    Trying to sync

--- NICE_Helper/NICE_Helper/Classes/FileWriter.cs
+++ NICE_Helper/NICE_Helper/Classes/FileWriter.cs
@@ -27,8 +27,7 @@ namespace NICE_Helper
 
         public void Compress(DirectoryInfo SourceFolder, string TargetFolder, bool InludeConfigs)
         {
-            try
-            {
+            try {
                 string newCompressedFile;
 
                 foreach (var fileToCompress in SourceFolder.GetFiles().Where(s => s.Name.ToLower().Contains(".log")))
@@ -56,7 +55,6 @@ namespace NICE_Helper
                 }
 
                 if (InludeConfigs)
-                {
                     foreach (var fileToCompress in SourceFolder.GetFiles().Where(s => s.Name.ToLower().Contains(".exe.config")))
                     {
                         using (FileStream originalFileStream = fileToCompress.OpenRead())
@@ -74,20 +72,17 @@ namespace NICE_Helper
                                 using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
                                 {
                                     originalFileStream.CopyTo(compressionStream);
-                                }
-                                
+                                }                                
                             }
                         }
                     }
-                }
             }
             catch (Exception) { throw; }
         }
 
         public int Uncompress(DirectoryInfo SourceFolder)
         {
-            try
-            {
+            try {
                 // Unzip using 7-zip
                 // Windows version:
                 //string zPath = @"C:\Program Files\7-Zip\7zG.exe";
